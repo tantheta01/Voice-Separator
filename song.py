839c920a6c93bd12281f066a79f70c73398eb332
greettanay@gmail.com <tantheta01>
2020-04-30 10:26:22 +0530

    Changed Songs File

--- song.py
+++ song.py
@@ -7,12 +7,12 @@ import pandas as pd
 
 def load_file(filepath ,sr = None):
 	# we shall get 2 channels as we read the filepath, i.e. 2 one dimensional arrays
-
-	channel1, sr = librosa.load(filepath, sr)
+	# librosa returns the array and the sampling rate. we get both the arrays if we set mono= False in load function, else we get mean of both arrays
+	channel, sampling_rate = librosa.load(filepath, sr)
 	# we shall process the 2 channels separately
 	stft_channel1 = librosa.stft(channel1)
-	# stft_channel2 = librosa.stft(channel2)
-	return stft_channel1, sr
+# 	stft_channel2 = librosa.stft(channel2)
+	return stft_channel1, sampling_rate
 
 
 def get_mag_phase(stft_channel):
