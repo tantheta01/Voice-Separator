5d9778e181131a4dbf4bd8fd41d2ffda097b6e86
greettanay@gmail.com <tantheta01>
2020-05-05 12:31:16 +0530

    Added general model

--- model.py
+++ model.py
@@ -2,17 +2,19 @@
 
 # Contains the model
 import librosa
-import keras
-import IPython as ip
-import numpy as np
-import tensorflow as tf
-from keras.layers import Dense
-from keras.layers import Flatten
-from keras.layers import MaxPool2D
+from keras.models import Sequential
 from keras.layers import Conv2D
 from keras.layers import Dropout
+from keras.layers import Flatten
 from keras.layers import Reshape
-from keras.layers import Deconvolution2D
+from keras.layers import Dense
+from keras.layers import UpSampling2D
+from keras.layers import Conv2DTranspose
+from keras.layers import BatchNormalization
+from keras.layers import MaxPool2D
+import numpy as np
+from keras.layers import BatchNormalization
+import keras
 import sys
 
 def custom_loss(y_actual,y_pred):
@@ -22,20 +24,41 @@ def custom_loss(y_actual,y_pred):
 	loss=tf.keras.backend.sum(loss)
 	return loss
 
-def model():
-	# these input dimensions are as of now 
-	# we may modify them later if need be
-	model=keras.models.Sequential()
-	model.add(Conv2D(50,(513,1),input_shape=(513,25,1)))
-	model.add(Conv2D(30,(1,12)))
-	model.add(Flatten())
-	model.add(Dense(128))
-	model.add(Dense(420))
-	model.add(Reshape((1,14,30)))
-	model.add(Deconvolution2D(50,(1,12)))
-	model.add(Deconvolution2D(1,(513,1)))
-	model.compile(optimizer='adadelta',loss=custom_loss, metrics=['accuracy'])
-	return model
+def build_model(conv1st=None,maxPooling=None,conv2nd=None,Connected=None,reshape=None,convT2nd=None,upSampling=None,convT1st=None,optimizer='adam',loss='mean_squared_error'):
+  classifier=Sequential()
+  if conv1st is not None:
+    classifier.add(Conv2D(conv1st[0], (conv1st[1],conv1st[2]),input_shape=(513,25,1),kernel_initializer='uniform'))
+  if maxPooling is not None:
+    classifier.add(MaxPool2D(pool_size=(maxPooling[0],maxPooling[1])))
+  if conv2nd is not None:
+    # adding padding(pata nhi kyu)
+    classifier.add(Conv2D(conv2nd[0],(conv2nd[1],conv2nd[2]),kernel_initializer='uniform', padding='same'))
+  classifier.add(Flatten())
+  if Dense is not None:
+    for layer in Connected:
+      classifier.add(Dense(layer,activation='relu',kernel_initializer='uniform'))
+      #adding extra batch normalization
+      # classifier.add(BatchNormalization())
+  if reshape is not None:
+    classifier.add(Reshape(reshape))
+  if convT2nd is not None:
+    classifier.add(Conv2DTranspose(convT2nd[0],(convT2nd[1],convT2nd[2])))
+  if upSampling is not None:
+    classifier.add(UpSampling2D(size=(upSampling[0],upSampling[1])))
+  if convT1st is not None:
+    classifier.add(Conv2DTranspose(convT1st[0],(convT1st[1],convT1st[2])))
+  classifier.compile(optimizer=optimizer, loss=loss,metrics=['accuracy'])
+  return classifier
+
+conv1st = (128,513,1)
+maxPooling=None
+conv2nd=(128,1,13)
+connected=[512,512,512,128*25]
+reshape=(1,25,128)
+convT2nd=None
+upSampling=None
+convT1st=(1,513,1)
+classifier1 = build_model(conv1st=conv1st, maxPooling=maxPooling,conv2nd=conv2nd,Connected=connected,reshape=reshape,convT2nd=convT2nd,upSampling=upSampling,convT1st=convT1st)
 
 
 def train_model(dataset, epochs):
@@ -52,9 +75,9 @@ def train_model(dataset, epochs):
 	else:
 		train_y = dataset['train_y']
 
-	regressor = model()
-	regressor.fit(train_x, train_y, epochs = epochs)
-	return regressor
+	
+	classifier1.fit(train_x, train_y, epochs = epochs)
+	return classifier1
 
 
 def test_model(dataset, model):
