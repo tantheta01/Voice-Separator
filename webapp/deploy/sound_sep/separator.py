a5a02cf0c66ae928a490d99103d67e600a88574b
sheelfshah <sheelfshah@gmail.com>
2020-06-15 15:43:13 +0530

    better flow, downloading added

---
+++ webapp/deploy/sound_sep/separator.py
@@ -0,0 +1,6 @@
+def separate(input_file_name):
+	input_file_path="media/"+input_file_name
+	#do separation
+	output_file_name=input_file_name
+	#save in "media/"+output_file_name
+	return output_file_name, output_file_name, output_file_name, output_file_name
--- webapp/deploy/sound_sep/urls.py
+++ webapp/deploy/sound_sep/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 
 urlpatterns = [
 	path(r'upload/', views.upload_file, name = 'upload'),
-
+	path("", views.homepage, name="homepage"),
+	path(r'download/', views.show_download_files, name = 'show_download'),
+	path(r'download/<str:path>', views.download_file, name="download_file"),
 	]
